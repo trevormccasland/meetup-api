e1dc15f76d64ef046806d443c68c20eedc75067b
Trevor McCasland <trevormccasland1@gmail.com>
2024-02-04 13:52:53 -0600

    spring quickstart

---
+++ src/test/java/com/trevormccasland/meetupapi/MeetupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevormccasland.meetupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
